0b69931394dd530653bdab8574509d8980796cd0
Chris Rabuse <rab233@live.com>
2016-03-26 20:41:18 -0500

    Completed functionality on Pro Mode

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Prime Weekend Challenge 1
 
-##Hey Primers!
+## Hey Primers!
 
 For this weekend, you will be creating an application that will process insurance claims. You will be provided an array of 5 items, where you will use the constructor function to create 5 more, combining to the two to have one array of 10 items.  
 
@@ -10,17 +10,17 @@ After that is complete, console out the entire amount paid out.
 
 Numbers should be rounded to the nearest whole number!
 
-###Rules
+### Rules
 Optical - covers 0%
 
 Specialist - covers 10%
 
 Emergency - covers 100%
 
-Primary Care - covers 50%	
+Primary Care - covers 50%
 
-##Hard Mode
+## Hard Mode
 Append to the DOM!
 
-##Pro Mode
+## Pro Mode
 Style appended content with CSS!
--- app.js
+++ app.js
@@ -28,16 +28,80 @@ var claim5 = {
 	visitCost: 770
 }
 
-var initialList = [claim1, claim2, claim3, claim4, claim5]
+var initialList = [claim1, claim2, claim3, claim4, claim5];
+
 
 var totalPayedOut = 0;
 
-function claim(name, type, cost){
+
+function Claim(name, type, cost){
 	this.patientName = name;
 	this.visitType = type;
 	this.visitCost = cost;
 }
 
-//function to determine percent covered
+// New claims using constructor
+var claim6 = new Claim('Tom Maverick', 'Specialist', 2395);
+var claim7 = new Claim('Dob Bod', 'Emergency', 7849949);
+var claim8 = new Claim('Ruby Diamond', 'Optical', 250);
+var claim9 = new Claim('Frumpy Noddle', 'Primary Care', 694);
+var claim10 = new Claim('Lesli Dunkle', 'Emergency', 13494);
+
+// New array containg claims 6-10
+var newClaims = [claim6, claim7, claim8, claim9, claim10];
+
+// Combine both arrays into a single variable
+var claims = initialList.concat(newClaims);
+
+// Cycle through all the claims to calculate the percentage and amount
+for (var i = 0; i < claims.length; i++) {
+	totalPayedOut += calcAmount(claims[i]);
+}
+
+// Append claim to DOM
+$(document).ready(function() {
+	$(document).find('body').append('<p class="total"><span>Total paid out: </span>' + formatAsCurrency(totalPayedOut) + '</p>');
+});
+
+// Output entire amount that was payed out
+console.log('Total paid out:', formatAsCurrency(totalPayedOut));
+
+//Determine percent covered
+function calcPercent(claim) {
+	switch(claim.visitType) {
+		case 'Optical':
+			return 0;
+			break;
+		case 'Specialist':
+			return 0.1;
+			break;
+		case 'Emergency':
+			return 1;
+			break;
+		case 'Primary Care':
+			return 0.5;
+	}
+}
 
 //function to determine amount covered
+function calcAmount(claim) {
+	var totalCovered =  calcPercent(claim) * claim.visitCost;
+
+	// Append claim to DOM
+	$(document).ready(function() {
+		$(document).find('body').append('<p class="claim">Paid out <span>' + formatAsCurrency(totalCovered) + '</span> for ' + claim.patientName + '</p>');
+	});
+
+	console.log('Paid out', formatAsCurrency(totalCovered), 'for', claim.patientName);
+
+	return totalCovered;
+}
+
+// Format input as currency
+function formatAsCurrency(input) {
+	return input.toLocaleString('en-US', {
+		style: 'currency',
+		currency: 'USD',
+		currencyDisplay: 'symbol'
+	});
+}
